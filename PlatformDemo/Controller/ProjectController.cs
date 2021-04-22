33483bdfdf935e996f672335f2686959a0cba063
Quan <quan.ma2018@gmail.com>
2021-04-21 23:24:41 -0400

    model binding

--- PlatformDemo/Controller/ProjectController.cs
+++ PlatformDemo/Controller/ProjectController.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace PlatformDemo.Controller
 {
-    public class ProjectController
+    [ApiController]
+    public class ProjectController : ControllerBase
     {
-        public ProjectController()
+        [HttpGet]
+        [Route("api/items")]
+        public IActionResult Get()
         {
+            return Ok("Got items");
+        }
+
+        [HttpPost]
+        [Route("api/item")]
+        public IActionResult Post()
+        {
+            return Ok("Created item");
+        }
+
+        [HttpPost]
+        [Route("/api/item/{id}/tickets")]
+        public IActionResult Post(string id, string tId)
+        {
+            return Ok($"created item {id} with ticket #{tId}");
+        }
+
+        [HttpPut]
+        [Route("api/item/{id}")]
+        public IActionResult Put(string id)
+        {
+            return Ok($"updating ticket {id}");
         }
     }
 }
